96a2f34202ae0df6b6bd50c89d6978bb64b8a65e
Akbar <akbar.h@samsung.com>
2020-04-13 22:21:56 +0530

    Added support for year in request

--- Data Collecter/fetchMovie.py
+++ Data Collecter/fetchMovie.py
@@ -4,19 +4,20 @@ import requests
 
 # Input:
 # data.csv --> movies name as a single column without header
-# 
+# year --> Manually a variable
+#
 # Output:
 # result2.csv --> Extracted data
 # moviesnotFount.csv --> Movies which dont have data in omdb API
 
-
+print("Enter the year of the movie collection : ", end="")
+inputYear = int(input())
 with open('data.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     for row in csv_reader:
         movie = row[0]
         movie.replace(" ","+")
-
-        url = "http://www.omdbapi.com/?t=" + movie + "&apikey=1cf7cfa6"
+        url = "http://www.omdbapi.com/?t=" + movie + "&y=" + str(inputYear) + "&apikey=1cf7cfa6"
         response = requests.get(url)
         responseData = json.loads(response.text)
         if len(responseData) == 0:
