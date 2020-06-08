e25c266bf85754cf0ca62f13f50a18dea4057cb4
viswa475 <viswavickymvs@gmail.com>
2020-06-08 08:32:39 +0530

    Logging Feature Added

--- app.py
+++ app.py
@@ -11,6 +11,11 @@ from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from github import Github
 import os,requests,json
+from logging.handlers import RotatingFileHandler
+from time import strftime
+import logging
+import traceback
+
 
 app = Flask(__name__)
 CORS(app)
@@ -134,5 +139,36 @@ def vicky():
     except Exception as e:
         return(str(e))
 
+@app.after_request
+def after_request(response):
+    if response.status_code != 500:
+        ts = strftime('[%Y-%b-%d %H:%M]')
+        logger.error('%s %s %s %s %s %s ',
+                      ts,
+                      request.remote_addr,
+                      request.method,
+                      request.scheme,
+                      request.full_path,
+                      response.status)
+    return response
+
+
+@app.errorhandler(Exception)
+def exceptions(e):
+    ts = strftime('[%Y-%b-%d %H:%M]')
+    tb = traceback.format_exc()
+    logger.error('%s %s %s %s %s 5xx INTERNAL SERVER ERROR\n%s',
+                  ts,
+                  request.remote_addr,
+                  request.method,
+                  request.scheme,
+                  request.full_path,
+                  tb)
+    return "Internal Server Error", 500
+
 if __name__ == '__main__':
+    handler = RotatingFileHandler('app.log', maxBytes=10000, backupCount=3)
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.WARNING)
+    logger.addHandler(handler)
     app.run()
